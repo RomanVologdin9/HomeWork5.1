993c05bb0669c416ffaffd1a1f6d784fa9d3dc2e
Vologdin Roman <vologdinroman@yandex.ru>
2025-07-28 21:20:43 +0800

    added string: scanner.close

--- src/Task1.java
+++ src/Task1.java
@@ -3,12 +3,13 @@ import java.util.Scanner; // Импорт класса Scanner для чтени
     public class Task1 {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in); // Создание объекта Scanner для чтения из стандартного ввода
-                char inputChar = scanner.nextLine().charAt(0); // Чтение строки из ввода и взятие первого символа
-                String keyboard = "qwertyuiopasdfghjklzxcvbnm"; // Задание строки, представляющей клавиатуру в виде кольца
-                int index = keyboard.indexOf(inputChar); // Поиск индекса введенного символа в строке клавиатуры
-                    // Вычисление индекса предыдущего символа с учетом замкнутости (кольца)
-                    // (index - 1) может быть отрицательным, поэтому прибавляем длину строки и берем по модулю длины
-                int prevIndex = (index - 1 + keyboard.length()) % keyboard.length();
-                System.out.println(keyboard.charAt(prevIndex)); // Вывод символа, находящегося слева от введенного
+            char inputChar = scanner.nextLine().charAt(0); // Чтение строки из ввода и взятие первого символа
+            String keyboard = "qwertyuiopasdfghjklzxcvbnm"; // Задание строки, представляющей клавиатуру в виде кольца
+            int index = keyboard.indexOf(inputChar); // Поиск индекса введенного символа в строке клавиатуры
+                // Вычисление индекса предыдущего символа с учетом замкнутости (кольца)
+                // (index - 1) может быть отрицательным, поэтому прибавляем длину строки и берем по модулю длины
+            int prevIndex = (index - 1 + keyboard.length()) % keyboard.length();
+            System.out.println(keyboard.charAt(prevIndex)); // Вывод символа, находящегося слева от введенного
+            scanner.close(); // Закрытие сканнера после окончания работы
+        }
     }
-}
